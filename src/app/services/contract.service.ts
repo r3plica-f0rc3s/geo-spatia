9a59213c6823c3755a3703a0c768587271c747e4
ohm0n <thecyberd3m0n@gmail.com>
2021-06-27 12:06:47 +0200

    installed harmony

---
+++ src/app/services/contract.service.ts
@@ -0,0 +1,29 @@
+import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import Web3 from 'web3';
+import abi from './abi/ABI.json';
+
+declare let window: any;
+
+@Injectable()
+export class ContractService {
+  currentWeb3: any;
+
+  errorSubject = new BehaviorSubject<string>('');
+  error$ = this.errorSubject.asObservable();
+
+  contractAddress = '0x844B9f34Ef221c5c29b406BDd068f4fAB71be211';
+  contract: any;
+  async init() {
+      const wallet = window.ethereum || window.onewallet;
+      if (!wallet) {
+          this.errorSubject.next('No supported wallet');
+      }
+
+      this.currentWeb3 = new Web3(wallet);
+      // Ask User permission to connect to Metamask
+    //   await window.ethereum.enable();
+      this.contract = this.currentWeb3.eth.Contract(abi,this.contractAddress);
+
+  }
+}
