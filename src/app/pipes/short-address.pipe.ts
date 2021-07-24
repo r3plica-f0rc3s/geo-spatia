87e31ee0c80ecb64835ce62c5aad7b6fd8a166a3
Dima <Dima>
2021-07-24 02:07:49 +0000

    Auto-generated commit [ci skip]

--- src/app/pipes/short-address.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({name: 'shortAddress'})
-export class ShortAddressPipe implements PipeTransform {
-  transform(value: string, ...args: any[]) {
-    return value ? `${value.substring(0, 4)}...${value.substr(value.length - 4)}` : '';
-  }
-
-}
